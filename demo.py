1e0d91304dde63b5e45241b19b6a15c3c05bd0da
Manasviganesan <126682235+Manasviganesan@users.noreply.github.com>
2025-09-29 23:42:48 +0530

    Add Python scripts with API and internal URLs https://api.demo-githubminer.com http://internal.demo-githubminer.com

---
+++ demo.py
@@ -0,0 +1,7 @@
+# Demo Python script for github_miner
+
+SUPPORT_EMAIL = "support@demo-githubminer.com"
+API_URL = "https://api.demo-githubminer.com"
+
+def greet_user():
+    print("Welcome to GitHub Miner Demo!")
